29fb34ab9795f9c296f587018ddc09ff74d11cdb
Zaheir <zaheir786@hotmail.com>
2019-10-17 11:28:07 -0400

    Add project files.

---
+++ gitproject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gitproject.MvcApplication" Language="C#" %>
---
+++ gitproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitproject.Startup))]
+namespace gitproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
